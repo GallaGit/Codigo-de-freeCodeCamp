2d56e45781258db1b2d631484103b7439ad521a3
Ociel Gallardo <ociel.galla@gmail.com>
2026-07-08 15:37:54 +0200

    Studio JS variables y cademas terminado

---
+++ JS/Lab/Bot-de-Trivia/index.js
@@ -0,0 +1,18 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "José";
+const botLocation = "your heart";
+const favoriteLanguage = "JavaScript";
+console.log("My name is " + botName + (" and I live on ") + botLocation + ("."));
+console.log("My favorite programming language is " + favoriteLanguage +("."));
+
+let codingFact = "Sabias que " + favoriteLanguage + " es el lenguaje de programacion mas antiguo y util de todos?"
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " es el lenguaje que mas usas dia a dia y ni siquiera sabes que esta ahi.";
+console.log(codingFact)
+
+codingFact = "asi de importante es " + favoriteLanguage +(".");
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! " + botName + " from " + botLocation + ("."));
---
+++ JS/Lab/creador-de-oraciones/index.js
@@ -0,0 +1,21 @@
+let adjective = "funny";
+let noun = "dragon";
+let verb = "jumping";
+let place = "garden";
+let adjective2 = "sparkling";
+let noun2 = "cakes";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "brave";
+noun = "wizard";
+verb = "dancing";
+place = "castle";
+adjective2 = "golden";
+noun2 = "cookies";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
