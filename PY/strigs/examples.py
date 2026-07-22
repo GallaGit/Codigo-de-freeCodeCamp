ca06de64e40f5a6f1f43ec7aa553ae6e86c97d0c
Ociel Gallardo <ociel.galla@gmail.com>
2026-07-22 18:00:50 +0200

    studing-python

---
+++ PY/strigs/examples.py
@@ -0,0 +1,10 @@
+my_str_3 = """Multiline
+string"""
+my_str_4 = '''Another
+multiline
+string'''
+print(my_str_3 , my_str_4)
+
+msg = "It's a sunny day"
+quote = 'She said, "Hello World!"'
+print(msg , quote)
